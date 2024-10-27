4423d09c1e3b9980c2730beeba3e6c9f65711e6f
vera3 <vera3899@gmail.com>
2024-10-27 22:01:19 +0300

    init dragon for rksp7

---
+++ src/main/java/com/verarchik/dragonsrksp7/DragonsRksp7Application.java
@@ -0,0 +1,13 @@
+package com.verarchik.dragonsrksp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsRksp7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonsRksp7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/verarchik/dragonsrksp7/DragonsRksp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verarchik.dragonsrksp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonsRksp7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
